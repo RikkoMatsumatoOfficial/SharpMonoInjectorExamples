7049683a3491c4e234990b5f6180ac8cb64eee5e
Rikko Matsumato <kuttimsx@hotmail.com>
2025-03-31 19:16:25 +0300

    Add files via upload

---
+++ SharpMonoInjectorSimpleExample/Example.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpMonoInjectorSimpleExample
+{
+    public class Example
+    {
+        static UnityEngine.GameObject gameObject;
+
+        public static void Load()
+        {
+            gameObject = new UnityEngine.GameObject();
+            gameObject.AddComponent<Loader>();
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
+        }
+
+        public static void Unload()
+        {
+            UnityEngine.Object.Destroy(gameObject);
+        }
+    }
+}
